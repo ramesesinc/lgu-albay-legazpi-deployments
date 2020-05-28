788b4f7ba417add787e5609bdb8b2044b215aea5
worgflores <worgflores@gmail.com>
2020-05-28 19:06:29 +0800

    updated

--- local-obo/dev_modules/rameses-gov-etracs-obo-services.jar/sql/building_application_subdoc.sql
+++
@@ -1,7 +0,0 @@
-[getSubdocsToIssue]
-SELECT sd.*, dt.template, dt.controlnopattern
-FROM building_application_subdoc sd 
-INNER JOIN building_doc_type dt ON sd.doctypeid=dt.objid
-WHERE sd.appid = $P{appid}
-AND sd.issuanceid IS NULL
-AND dt.issuetype = 3
