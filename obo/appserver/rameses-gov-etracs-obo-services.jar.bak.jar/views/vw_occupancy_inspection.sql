9eef543761c2fa23850790cdfd22b79686309256
elmonazareno <elmonazareno@gmail.com>
2020-06-08 09:10:14 +0800

    updated obo services with file attachment in findings

--- obo/appserver/rameses-gov-etracs-obo-services.jar.bak.jar/views/vw_occupancy_inspection.sql
+++
@@ -1,8 +0,0 @@
-DROP VIEW IF EXISTS vw_occupancy_inspection;
-CREATE VIEW vw_occupancy_inspection AS 
-SELECT 
-   a.objid,
-   a.title,
-   e.typeid
-FROM obo_itemaccount a 
-INNER JOIN occupancy_inspection_type_itemaccount e ON a.objid = e.itemid 
---
+++ obo/migrations/20200529_update_finding.sql
@@ -0,0 +1,3 @@
+
+ALTER TABLE building_evaluation_finding ADD COLUMN attachment_fileid varchar(255);
+ALTER TABLE building_evaluation_finding ADD COLUMN attachment_title varchar(255);
