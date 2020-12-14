3af9cef94b729eed0219ada6460690b2d348d073
worgflores <worgflores@gmail.com>
2020-12-14 10:55:48 +0800

    updated

---
+++ _res/patches/v255_20201214_001.patch/mssql/01-patch-etracs-rpt.sql
@@ -0,0 +1,59 @@
+if exists(select * from sysobjects where id = object_id('vw_rptcertification_item')) 
+begin 
+  drop view vw_rptcertification_item
+end 
+go 
+
+create view vw_rptcertification_item
+as 
+SELECT 
+  rci.rptcertificationid,
+  f.objid as faasid,
+  f.fullpin, 
+  f.tdno,
+  e.objid as taxpayerid,
+  e.name as taxpayer_name, 
+  f.owner_name, 
+  f.administrator_name,
+  f.titleno,  
+  f.rpuid, 
+  pc.code AS classcode, 
+  pc.name AS classname,
+  so.name AS lguname,
+  b.name AS barangay, 
+  r.rputype, 
+  r.suffix,
+  r.totalareaha AS totalareaha,
+  r.totalareasqm AS totalareasqm,
+  r.totalav,
+  r.totalmv, 
+  rp.street,
+  rp.blockno,
+  rp.cadastrallotno,
+  rp.surveyno,
+  r.taxable,
+  f.effectivityyear,
+  f.effectivityqtr
+FROM rptcertificationitem rci 
+  INNER JOIN faas f ON rci.refid = f.objid 
+  INNER JOIN rpu r ON f.rpuid = r.objid 
+  INNER JOIN propertyclassification pc ON r.classification_objid = pc.objid 
+  INNER JOIN realproperty rp ON f.realpropertyid = rp.objid 
+  INNER JOIN barangay b ON rp.barangayid = b.objid 
+  INNER JOIN sys_org so on f.lguid = so.objid 
+  INNER JOIN entity e on f.taxpayer_objid = e.objid 
+go
+
+
+/* PREVTAXABILITY */
+alter table faas_previous add prevtaxability varchar(10)
+go
+
+
+update pf set 
+  pf.prevtaxability = case when r.taxable = 1 then 'TAXABLE' else 'EXEMPT' end 
+from faas_previous pf, faas f, rpu r
+where pf.prevfaasid = f.objid
+and f.rpuid = r.objid 
+and pf.prevtaxability is null 
+go 
