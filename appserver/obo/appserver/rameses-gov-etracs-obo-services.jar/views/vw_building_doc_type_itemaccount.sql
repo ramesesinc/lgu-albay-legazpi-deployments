a8bec2b29ad69a5cbdcf90e763cd147d104e5421
worgflores <worgflores@gmail.com>
2021-02-08 18:44:44 +0800

    appserver: obo: updated

--- appserver/obo/appserver/rameses-gov-etracs-obo-services.jar/views/vw_building_doc_type_itemaccount.sql
+++
@@ -1,9 +0,0 @@
-DROP VIEW IF EXISTS vw_building_doc_type_itemaccount; 
-CREATE VIEW vw_building_doc_type_itemaccount AS 
-SELECT 
-   a.objid,
-   a.title,
-   e.typeid 
-FROM obo_itemaccount a 
-INNER JOIN building_doc_type_itemaccount e ON a.objid = e.itemid 
-
